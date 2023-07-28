5f82b9c2d84836349aac15721a68804616377d44
Gursevak56 <gursevaksinghgill21@gmail.com>
2023-07-28 13:11:19 +0530

    fjkdf

--- middleware/emailVerification.js
+++ middleware/emailVerification.js
@@ -8,5 +8,27 @@ const transporter = nodemailer.createTransport({
         user:"gursevaksinghgill21@gmail.com",
         pass:process.env.GMAIL_PASS
     }
-    
 })
+const mailOptions = {
+    from:"gursevaksgill@nexgeniots.com",
+    to:email,
+    subject:'Email verification',
+    html:'<p> Hii '+name+' This is otp for email verification </p>'
+}
+transporter.sendMail(mailOptions,(err,value,next)=>{
+    if(err){
+        next(err)
+    }
+    else{
+        console.log('email sent successfully')
+    }
+})
+const genrateotp =  ()=>{
+    let otplength = 6;
+   let otp ='';
+    for(let i =1;i<6;i++){
+    otp = otp + Math.floor(Math.random() *  10);
+    }
+    return otp;
+}
+console.log(genrateotp());
